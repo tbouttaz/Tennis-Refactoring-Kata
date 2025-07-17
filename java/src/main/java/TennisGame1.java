9ef5a5e49baa5785d41da7d5d70f7ee6d7ab3169
Tom Bouttaz <thomas_bouttaz@dell.com>
2025-07-17 14:58:30 +0100

    Extract handlePlayerAboutToWin() and handleRegularScore()

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -19,34 +19,44 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        String score;
         if (isSameScore()) {
-            score = handleSameScoreForBothPlayers();
-        } else if (playerOneScore >= 4 || playerTwoScore >= 4) {
-            int minusResult = playerOneScore - playerTwoScore;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
+            return handleSameScoreForBothPlayers();
+        } else if (isAnyPlayerCloseToWin()) {
+            return handlePlayerAboutToWin();
         } else {
-            StringBuilder scoreBuilder = new StringBuilder();
-            int tempScore = 0;
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = playerOneScore;
-                else {
-                    scoreBuilder.append("-");
-                    tempScore = playerTwoScore;
-                }
-                switch (tempScore) {
-                    case 0 -> scoreBuilder.append("Love");
-                    case 1 -> scoreBuilder.append("Fifteen");
-                    case 2 -> scoreBuilder.append("Thirty");
-                    case 3 -> scoreBuilder.append("Forty");
-                }
-            }
-            score = scoreBuilder.toString();
+            return handleRegularScore();
         }
-        return score;
+    }
+
+    private String handleRegularScore() {
+        StringBuilder scoreBuilder = new StringBuilder();
+        int tempScore = 0;
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) tempScore = playerOneScore;
+            else {
+                scoreBuilder.append("-");
+                tempScore = playerTwoScore;
+            }
+            switch (tempScore) {
+                case 0 -> scoreBuilder.append("Love");
+                case 1 -> scoreBuilder.append("Fifteen");
+                case 2 -> scoreBuilder.append("Thirty");
+                case 3 -> scoreBuilder.append("Forty");
+            }
+        }
+        return scoreBuilder.toString();
+    }
+
+    private String handlePlayerAboutToWin() {
+        int minusResult = playerOneScore - playerTwoScore;
+        if (minusResult == 1) return "Advantage player1";
+        else if (minusResult == -1) return "Advantage player2";
+        else if (minusResult >= 2) return "Win for player1";
+        else return "Win for player2";
+    }
+
+    private boolean isAnyPlayerCloseToWin() {
+        return playerOneScore >= 4 || playerTwoScore >= 4;
     }
 
     private boolean isSameScore() {
